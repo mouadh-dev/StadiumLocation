7845e0346962ea4d1730e4f6ae7f776426e88d95
mouadh-dev <mouadh.laa@gmail.com>
2022-01-20 20:36:06 +0100

    transfert to laravel

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -2,35 +2,33 @@
 <html>
     <head>
 		<title></title>
-		<link rel="stylesheet" href="styleFirstPage.css">
+		<!-- <link rel="stylesheet" href="styleFirstPage.css"> -->
 		<meta charset=utf-8>
 		<meta name=viewport content=width=device-width,initial-scale=1.0>
+        <link href="{{ asset('css/styleFirstPage.css') }}" rel="stylesheet">
 	</head>
     <body class="body">
-
-        
-        
-        <header>
+<header>
             <nav>
                 <h1 id="title">Stadna</h1><br>
             </nav>
             <section>
                 <div class="beginingImage">
-                   <img src="stadium.jpg" alt=""/>
+                   <img src="{{ asset('stadium.jpg') }}" alt=""/>
                    <div class="align-right">
                    <button class="buttonStart" type="button" style="position: absolute; right: 0;"><span>let's Start</span></button>
-                  
+
                 </div>
                 </div>
             </section>
         </header>
-        
+
         <footer>
             <div class="divAboutus">
             <p class="aboutUs">About us</p>
         </div>
             <br>
-           
+
             <div class="ContactUs">
                 <p>Contact US</p>
                 <br>
@@ -46,6 +44,6 @@
         <div class="slider"></div>
         <script src="https://cdn.bootcdn.net/ajax/libs/gsap/1.20.2/TweenMax.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TimelineMax.min.js"></script>
-        <script src="FirstScript.js"></script>
+        <script src="{{ asset('firstscript.js') }}" type="text/javascript"></script>
     </body>
 </html>
